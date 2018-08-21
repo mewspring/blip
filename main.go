db40fe5adaf1aca99b2eed1047687f5ee73dc349
mewmew <rnd0x00@gmail.com>
2018-08-22 00:35:45 +0900

    blip: add -dur flag to specify duration between rebuffering of samples

--- main.go
+++ main.go
@@ -12,10 +12,18 @@ import (
 	"github.com/faiface/beep/speaker"
 )
 
+func usage() {
+	fmt.Fprintln(os.Stderr, "Usage: blip [OPTIONS]... FILE")
+	flag.PrintDefaults()
+}
+
 func main() {
+	var dur time.Duration
+	flag.DurationVar(&dur, "dur", time.Second/10, "duration between rebuffering of samples")
+	flag.Usage = usage
 	flag.Parse()
 	if flag.NArg() != 1 {
-		fmt.Fprintf(os.Stderr, "Usage: blip FILE\n")
+		flag.Usage()
 		os.Exit(1)
 	}
 	fr, err := os.Open(flag.Arg(0))
@@ -23,7 +31,7 @@ func main() {
 		log.Fatalf("unable to open file; %v", err)
 	}
 	s, format, err := flac.Decode(fr)
-	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	speaker.Init(format.SampleRate, format.SampleRate.N(dur))
 	done := make(chan struct{})
 	f := func() {
 		close(done)
